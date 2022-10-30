4f04f506c8d49d955e9f4343b49c0818dc5854ec
leejanghe <babybye777@gmail.com>
2022-10-31 00:21:56 +0900

    btn 클릭시 이동 함수

--- study/practice/index.js
+++ study/practice/index.js
@@ -1,1 +1,8 @@
 console.log("ggss");
+
+// 버튼 클릭시 컨텐츠 5번 이동
+const btn = document.querySelector(".btn");
+const contents = document.querySelector(".test4");
+btn.addEventListener("click", () => {
+  contents.scrollIntoView({ behavior: "smooth" });
+});
